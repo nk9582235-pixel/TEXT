0ceca852e81c8e977a07a8447df1f7df06bdd4fd
nk9582235-pixel <nk9582235@gmail.com>
2025-11-09 19:44:02 +0530

    main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Config(object):
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-1003235364017').split(',')
+    GROUPS = os.environ.get('GROUPS', '3268370656').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
